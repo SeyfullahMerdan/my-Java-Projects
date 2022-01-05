a47bb37ed824ad9d8e94323c5ae376dc99524e16
seyfullah merdan <merdanseyfullah33@gmail.com>
2022-01-05 17:17:06 +0100

    scrabblegame

---
+++ src/scrabble_game/Scrabble.java
@@ -0,0 +1,21 @@
+package scrabble_game;
+
+public class Scrabble {
+	
+	@SuppressWarnings({ })
+	public static void main(String[] args) {
+
+		
+		Oyuncu oyuncular=new Oyuncu();
+		
+		
+		oyuncular.oyunuBaslat();
+		oyuncular.ikinciOyuncu(Oyuncu.kelime);
+		
+	
+		
+	}
+
+
+	
+}
